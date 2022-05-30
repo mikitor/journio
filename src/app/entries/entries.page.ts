efef542077301538ba0005890c6149cacd2ca462
Miklos Torbics <miklos.torbics@gmail.com>
2022-05-30 20:53:16 +0200

    feature/display-list-of-entries: Get entries into the Entries page

--- src/app/entries/entries.page.ts
+++ src/app/entries/entries.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EntriesService } from './entries.service';
+import { Entry } from './entry.model';
+
 @Component({
   selector: 'app-entries',
   templateUrl: './entries.page.html',
   styleUrls: ['./entries.page.scss'],
 })
 export class EntriesPage implements OnInit {
+  entries: Entry[];
 
-  constructor() { }
+  constructor(private entriesService: EntriesService) { }
 
   ngOnInit() {
+    this.entries = this.entriesService.getEntries();
   }
-
 }
