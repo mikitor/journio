01b2e9aec02ff75682a2c4e2b29e7c397d57a25f
Miklos Torbics <miklos.torbics@gmail.com>
2022-05-30 19:52:56 +0200

    feature/display-list-of-entries: Delete the Home component and clean up the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'entries',
     pathMatch: 'full'
   },
   {
     path: 'entries',
-    loadChildren: () => import('./entries/entries.module').then( m => m.EntriesPageModule)
+    loadChildren: () => import('./entries/entries.module').then(m => m.EntriesPageModule)
   },
 ];
 
