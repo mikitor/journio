282c5fb615ea986145af23634d14487840168343
Miklos Torbics <miklos.torbics@gmail.com>
2022-06-01 00:07:23 +0200

    feature/add-firestore-integration: Add firestore configuration files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'journio-6853e',
+    appId: '1:177914979699:web:1e3548cf7c624fc8fe5106',
+    storageBucket: 'journio-6853e.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBPC_kNwLseZcBymoP4GCVdmDF4VECUdcA',
+    authDomain: 'journio-6853e.firebaseapp.com',
+    messagingSenderId: '177914979699',
+  },
   production: true
 };
