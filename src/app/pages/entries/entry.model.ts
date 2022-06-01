a23d30c9cddb1aaebfd8cb0e5cba7daae349ae46
Miklos Torbics <miklos.torbics@gmail.com>
2022-06-01 21:39:44 +0200

    feature/add-entry-editing: Add timestamp to the Entry model

--- src/app/pages/entries/entry.model.ts
+++ src/app/pages/entries/entry.model.ts
@@ -1,7 +1,10 @@
+import { Timestamp } from '@angular/fire/firestore';
+
 export class Entry {
   constructor(
     public id: string,
     public content: string,
-    public description: string
+    public description: string,
+    public timestamp: Timestamp
   ) { }
 }
